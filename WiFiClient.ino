27cc32f3cfd42785bb8f8f5d05066396921b5af9
zarigata <39637452+zarigata@users.noreply.github.com>
2022-07-04 18:15:34 -0700

    Add files via upload

---
+++ WiFiClient.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+#ifndef STASSID
+#define STASSID "REDACTED"
+#define STAPSK  "REDACTED"
+#endif
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+const char* ssid     = STASSID;
+const char* password = STAPSK;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  // Display static text
+  display.println("CONNECTED");
+  display.display(); 
+}
+
+void loop() {
+
+ display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 20);
+  // Display static text
+  display.println(WiFi.localIP());
+  display.display(); 
+
+
+
+  delay (50000);
+}
